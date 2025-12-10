be8f75b3ec2088130fc85a7af2e2613aa5571300
Akib-Hosen <akibtonay2016@gmail.com>
2025-12-10 14:29:06 +0600

    php fix

--- php/labtask9.php
+++ php/labtask9.php
@@ -1,132 +1,238 @@
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>PHP Form Validation</title>
+    <title>Lab 3.2 PHP Validation</title>
+    <style>
+    body {
+        font-family: sans-serif;
+    }
+
+    .container {
+        border: 1px solid #000;
+        padding: 10px;
+        width: 350px;
+        margin-bottom: 20px;
+    }
+
+    .error {
+        color: red;
+        font-size: 0.9em;
+    }
+
+    .success {
+        color: green;
+        font-size: 0.9em;
+    }
+
+    fieldset {
+        border: 1px solid #ccc;
+        padding: 10px;
+    }
+
+    legend {
+        font-weight: bold;
+    }
+    </style>
 </head>
 
 <body>
 
-    <h2>LAB 3.2 – Form Validation</h2>
+    <div class="container">
+        <fieldset>
+            <legend>NAME</legend>
+            <form method="post" action="">
+                <input type="text" name="name" value="<?php echo isset($_POST['name']) ? $_POST['name'] : ''; ?>"><br>
+                <hr>
+                <input type="submit" name="submit_name" value="Submit">
+            </form>
+        </fieldset>
+        <?php
+        if (isset($_POST['submit_name'])) {
+            $name = trim($_POST['name']);
+            
+            
+            if (empty($name)) {
+                echo "<p class='error'>Error: Name cannot be empty.</p>";
+            } 
+            
+            elseif (str_word_count($name) < 2) {
+                echo "<p class='error'>Error: Name must contain at least two words.</p>";
+            } 
+           
+            elseif (!ctype_alpha($name[0])) {
+                echo "<p class='error'>Error: Name must start with a letter.</p>";
+            } 
+            
+            
+            elseif (!preg_match('/^[a-zA-Z0-9\.\-\s]+$/', $name)) { 
+                echo "<p class='error'>Error: Name can only contain letters, periods, and dashes.</p>";
+            } 
+            else {
+                echo "<p class='success'>Success: Valid Name.</p>";
+            }
+        }
+        ?>
+    </div>
 
-    <form method="post">
+    <div class="container">
+        <fieldset>
+            <legend>EMAIL</legend>
+            <form method="post" action="">
+                <input type="text" name="email"
+                    value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>"><br>
+                <hr>
+                <input type="submit" name="submit_email" value="Submit">
+            </form>
+        </fieldset>
+        <?php
+        if (isset($_POST['submit_email'])) {
+            $email = trim($_POST['email']);
+            
+            
+            if (empty($email)) {
+                echo "<p class='error'>Error: Email cannot be empty.</p>";
+            } 
+           
+            elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo "<p class='error'>Error: Invalid email format.</p>";
+            } 
+            else {
+                echo "<p class='success'>Success: Valid Email.</p>";
+            }
+        }
+        ?>
+    </div>
 
-        <label>Name:</label>
-        <input type="text" name="name" value="<?php echo $name; ?>">
-        <span style="color:red;">* <?php echo $nameErr; ?></span>
-        <br><br>
+    <div class="container">
+        <fieldset>
+            <legend>DATE OF BIRTH</legend>
+            <form method="post" action="">
+                <table>
+                    <tr>
+                        <td align="center">dd</td>
+                        <td align="center">mm</td>
+                        <td align="center">yyyy</td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="dd" style="width: 40px;"
+                                value="<?php echo isset($_POST['dd']) ? $_POST['dd'] : ''; ?>"> /</td>
+                        <td><input type="text" name="mm" style="width: 40px;"
+                                value="<?php echo isset($_POST['mm']) ? $_POST['mm'] : ''; ?>"> /</td>
+                        <td><input type="text" name="yyyy" style="width: 60px;"
+                                value="<?php echo isset($_POST['yyyy']) ? $_POST['yyyy'] : ''; ?>"></td>
+                    </tr>
+                </table>
+                <hr>
+                <input type="submit" name="submit_dob" value="Submit">
+            </form>
+        </fieldset>
+        <?php
+        if (isset($_POST['submit_dob'])) {
+            $dd = $_POST['dd'];
+            $mm = $_POST['mm'];
+            $yyyy = $_POST['yyyy'];
 
-        <label>Email:</label>
-        <input type="text" name="email" value="<?php echo $email; ?>">
-        <span style="color:red;">* <?php echo $emailErr; ?></span>
-        <br><br>
+           
+            if (empty($dd) || empty($mm) || empty($yyyy)) {
+                echo "<p class='error'>Error: Date fields cannot be empty.</p>";
+            } 
+           
+            elseif (!is_numeric($dd) || !is_numeric($mm) || !is_numeric($yyyy)) {
+                echo "<p class='error'>Error: Inputs must be numbers.</p>";
+            }
+            elseif ($dd < 1 || $dd > 31) {
+                echo "<p class='error'>Error: Day must be between 1 and 31.</p>";
+            }
+            elseif ($mm < 1 || $mm > 12) {
+                echo "<p class='error'>Error: Month must be between 1 and 12.</p>";
+            }
+            elseif ($yyyy < 1953 || $yyyy > 1998) {
+                echo "<p class='error'>Error: Year must be between 1953 and 1998.</p>";
+            }
+            else {
+                echo "<p class='success'>Success: Valid Date of Birth.</p>";
+            }
+        }
+        ?>
+    </div>
 
-        <label>Date of Birth:</label><br>
-        <input type="text" name="dd" size="2" placeholder="dd" value="<?php echo $dd; ?>">
-        <input type="text" name="mm" size="2" placeholder="mm" value="<?php echo $mm; ?>">
-        <input type="text" name="yyyy" size="4" placeholder="yyyy" value="<?php echo $yyyy; ?>">
-        <span style="color:red;">* <?php echo $dobErr; ?></span>
-        <br><br>
+    <div class="container">
+        <fieldset>
+            <legend>GENDER</legend>
+            <form method="post" action="">
+                <input type="radio" name="gender" value="Male"> Male
+                <input type="radio" name="gender" value="Female"> Female
+                <input type="radio" name="gender" value="Other"> Other
+                <hr>
+                <input type="submit" name="submit_gender" value="Submit">
+            </form>
+        </fieldset>
+        <?php
+        if (isset($_POST['submit_gender'])) {
+            
+            if (!isset($_POST['gender'])) {
+                echo "<p class='error'>Error: Please select a gender.</p>";
+            } else {
+                echo "<p class='success'>Success: You selected " . $_POST['gender'] . ".</p>";
+            }
+        }
+        ?>
+    </div>
 
-        <label>Gender:</label><br>
-        <input type="radio" name="gender" value="Male"> Male
-        <input type="radio" name="gender" value="Female"> Female
-        <input type="radio" name="gender" value="Other"> Other
-        <span style="color:red;">* <?php echo $genderErr; ?></span>
-        <br><br>
+    <div class="container">
+        <fieldset>
+            <legend>DEGREE</legend>
+            <form method="post" action="">
+                <input type="checkbox" name="degree[]" value="SSC"> SSC
+                <input type="checkbox" name="degree[]" value="HSC"> HSC
+                <input type="checkbox" name="degree[]" value="BSc"> BSc
+                <input type="checkbox" name="degree[]" value="MSc"> MSc
+                <hr>
+                <input type="submit" name="submit_degree" value="Submit">
+            </form>
+        </fieldset>
+        <?php
+        if (isset($_POST['submit_degree'])) {
+            
+            if (!isset($_POST['degree']) || count($_POST['degree']) < 2) {
+                echo "<p class='error'>Error: Please select at least two degrees.</p>";
+            } else {
+                echo "<p class='success'>Success: Valid Selection.</p>";
+            }
+        }
+        ?>
+    </div>
 
-        <label>Degree:</label><br>
-        <input type="checkbox" name="degree[]" value="SSC"> SSC
-        <input type="checkbox" name="degree[]" value="HSC"> HSC
-        <input type="checkbox" name="degree[]" value="BSc"> BSc
-        <input type="checkbox" name="degree[]" value="MSc"> MSc
-        <span style="color:red;">* <?php echo $degreeErr; ?></span>
-        <br><br>
-
-        <label>Blood Group:</label>
-        <select name="blood">
-            <option value="">Select</option>
-            <option>A+</option>
-            <option>A-</option>
-            <option>B+</option>
-            <option>B-</option>
-            <option>O+</option>
-            <option>O-</option>
-            <option>AB+</option>
-            <option>AB-</option>
-        </select>
-        <span style="color:red;">* <?php echo $bloodErr; ?></span>
-        <br><br>
-
-        <input type="submit" value="Submit">
-
-    </form>
+    <div class="container">
+        <fieldset>
+            <legend>BLOOD GROUP</legend>
+            <form method="post" action="">
+                <select name="blood_group">
+                    <option value="">Select</option>
+                    <option value="A+">A+</option>
+                    <option value="B+">B+</option>
+                    <option value="O+">O+</option>
+                    <option value="AB+">AB+</option>
+                </select>
+                <hr>
+                <input type="submit" name="submit_bg" value="Submit">
+            </form>
+        </fieldset>
+        <?php
+        if (isset($_POST['submit_bg'])) {
+            $bg = $_POST['blood_group'];
+            
+            
+            if (empty($bg)) {
+                echo "<p class='error'>Error: Please select a blood group.</p>";
+            } else {
+                echo "<p class='success'>Success: Valid Blood Group ($bg).</p>";
+            }
+        }
+        ?>
+    </div>
 
 </body>
 
 </html>
-
-
-<?php
-
-$nameErr = $emailErr = $dobErr = $genderErr = $degreeErr = $bloodErr = "";
-$name = $email = $dd = $mm = $yyyy = $gender = $blood = "";
-$degree = [];
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $name = trim($_POST["name"]);
-
-    if (empty($name)) {
-        $nameErr = "Name cannot be empty";
-    } else {
-        if (str_word_count($name) < 2) {
-            $nameErr = "Name must contain at least two words";
-        }
-        else if (!preg_match("/^[A-Za-z]/", $name)) {
-            $nameErr = "Name must start with a letter";
-        }
-        else if (!preg_match("/^[A-Za-z.\- ]+$/", $name)) {
-            $nameErr = "Only letters, periods, dash allowed";
-        }
-    }
-    $email = trim($_POST["email"]);
-
-    if (empty($email)) {
-        $emailErr = "Email cannot be empty";
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $emailErr = "Invalid email format";
-    }
-
-    $dd = $_POST["dd"];
-    $mm = $_POST["mm"];
-    $yyyy = $_POST["yyyy"];
-
-    if (empty($dd) || empty($mm) || empty($yyyy)) {
-        $dobErr = "Date of birth cannot be empty";
-    } else {
-        if ($dd < 1 || $dd > 31 || $mm < 1 || $mm > 12 || $yyyy < 1953 || $yyyy > 1998) {
-            $dobErr = "Invalid date format (dd:1-31, mm:1-12, yyyy:1953-1998)";
-        }
-    }
-
-    if (empty($_POST["gender"])) {
-        $genderErr = "At least one gender must be selected";
-    } else {
-        $gender = $_POST["gender"];
-    }
-
-    if (empty($_POST["degree"])) {
-        $degreeErr = "Select at least two degrees";
-    } else {
-        $degree = $_POST["degree"];
-        if (count($degree) < 2) {
-            $degreeErr = "Select at least two degrees";
-        }
-    }
-    if (empty($_POST["blood"])) {
-        $bloodErr = "Blood group must be selected";
-    } else {
-        $blood = $_POST["blood"];
-    }
-}
-?>
